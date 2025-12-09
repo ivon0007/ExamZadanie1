5df20be20ee008f7eb53a5bc1f021a186ef2bc65
ivon0007 <stupapupa563@gmail.com>
2025-12-09 11:14:02 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace MathAndStringTests
+{
+    // Класс для реализации тестов со строковыми функциями
+    public class StringOperations
+    {
+        public string ConvertToLowerCase(string input) => input.ToLower();
+    }
+
+    // ЗАДАНИЕ 10: Преобразование строки в нижний регистр
+    public class StringOperationsTests
+    {
+        private readonly StringOperations _stringOps = new StringOperations();
+
+        [Fact]
+        public void Test10_ConvertToLowerCase()
+        {
+            string input = "HELLO WORLD";
+            string expected = "hello world";
+
+            string result = _stringOps.ConvertToLowerCase(input);
+
+            Assert.Equal(expected, result);
+            Assert.True(result.All(c => !char.IsUpper(c))); // Дополнительная проверка
+        }
+    }
+}
